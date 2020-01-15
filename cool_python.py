5aaced20277d315d890c41319db5d928bcd7c860
Jalo Javascript <jalo.javascript@tuni.fi>
2020-01-15 15:20:21 +0200

    Fix model

--- cool_python.py
+++ cool_python.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=201)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
@@ -14,6 +14,6 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=201)
-    voites = models.IntegerField(default=0)
+    votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
