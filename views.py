75e2011974124ed9cef819f8215702a86a8adf88
Pertti Python <pertti.python@tuni.fi>
2020-01-16 07:21:05 +0200

    Add first view

---
+++ views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
