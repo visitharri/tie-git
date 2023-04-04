531bb6fb13be100e5f1e1b3bd1f105abdbca98b4
Harri Hietanen <harri.hietanen@tuni.fi>
2023-04-04 22:33:59 +0300

    Tie-GIT Basic Exercises: File version return

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+"""
+Course: Tie-GIT
+Name: Harri Hietanen
+Email: harri.hietanen@tuni.fi
+Student number: k56521
+Description: Basic Exercises: Hello World
+
+"""
+
+
+def main():
+    """
+    TODO: write your code here:
+    """
+    print("Hello World!")
+
+
+if __name__ == '__main__':
+    main()
