85dd10db16647bdbc600b1d87acb6452d4fc66ed
Harri Hietanen <harri.hietanen@tuni.fi>
2023-04-10 00:14:43 +0300

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+"""
+Course: Tie-GIT
+Name: Harri Hietanen
+Email: harri.hietanen@tuni.fi
+Student number: k56521
+Description: Basic Exercises: Hello World
+
+"""
+
+
+def main():
+    """
+    TODO: write your code here:
+    """
+    print("Hello world!")
+
+
+
+if __name__ == '__main__':
+    main()
